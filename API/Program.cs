44764f7050c48c6847e01a04c8ff50528a4669e1
Aayu2000 <upadhyayaayush989@gmail.com>
2026-05-05 22:32:55 +0530

    Implementation till Section 4:30

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 
+using Application.Activities.Queries;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
 
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
